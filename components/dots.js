bf9e4c95084b42306c19fa290db9351c8179d40d
kpoddubnyi <kpoddubnyi@justcoded.co>
2017-06-13 17:20:13 +0300

    Create dots component

--- components/dots.js
+++ components/dots.js
@@ -1,57 +1,34 @@
 AFRAME.registerComponent('dots', {
   schema: {
-    radius: {
+    rows: {
       type: 'number',
-      default: 0.1
-    },
-    widthSegments: {
-      type: 'number',
-      default: 32
-    },
-    heightSegments: {
-      type: 'number',
-      default: 32
-    },
-    color: {
-      type: 'color',
-      default: '#000'
+      default: 5
     },
     elements: {
       type: 'number',
-      default: 10
+      default: 5
     }
   },
   init: function () {
-    var data = this.data;
-    var el = this.el;
+    var element = this.el;
 
-    this.geometry = new THREE.SphereBufferGeometry(data.radius, data.widthSegments, data.heightSegments);
-    this.material = new THREE.MeshStandardMaterial({
-      color: data.color
-    });
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    el.setObject3D('mesh', this.mesh);
+    element.insertAdjacentHTML('afterBegin', this.renderDots());
   },
-  /**
-   * Update the mesh in response to property updates.
-   */
-  update: function (oldData) {
-    var data = this.data;
-    var el = this.el;
-    // If `oldData` is empty, then this means we're in the initialization process.
-    // No need to update.
-    if (Object.keys(oldData).length === 0) {
-      return;
+  renderDots: function () {
+    rows = this.data.rows,
+      elements = this.data.elements,
+      geometry = this.el.getAttribute('geometry'),
+      widthStepValue = geometry.width / rows,
+      depthStepValue = geometry.depth / elements,
+      allDots = "";
+
+    for (var i = 0; i < rows; i++) {
+      for (var j = 0; j < elements; j++) {
+        allDots += '<a-entity mixin=\"dot\" position=\"' + i * widthStepValue + ' 0 ' + j * depthStepValue + '\"></a-entity>';
+        console.log(`row : ${i} ; element : ${j} ; widthStepValue : ${i * widthStepValue}; depthStepValue : ${j * depthStepValue}`);
+      }
     }
-    // Geometry-related properties changed. Update the geometry.
-    if (data.radius !== oldData.radius ||
-      data.widthSegments !== oldData.widthSegments ||
-      data.heightSegments !== oldData.heightSegments) {
-      el.getObject3D('mesh').geometry = new THREE.BoxBufferGeometry(data.radius, data.widthSegments, data.heightSegments);
-    }
-    // Material-related properties changed. Update the material.
-    if (data.color !== oldData.color) {
-      el.getObject3D('mesh').material.color = data.color;
-    }
+
+    return allDots;
   }
 });
